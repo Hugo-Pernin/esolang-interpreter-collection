cb93358eba46846fdcd6ccd9e1e1a359be087382
Hugo-Pernin <hugopernin71@gmail.com>
2025-10-26 14:34:43 +0100

    Add brainfuck file opening

--- brainfuck/brainfuck.c
+++ brainfuck/brainfuck.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
+const int MAX_PATH_LENGTH = 4096;
+
 int main() {
-    printf("Hello world!");
+    printf("----- Brainfuck interpreter -----\n");
+    printf("Please enter your file path: ");
+    
+    char path[MAX_PATH_LENGTH];
+    scanf("%s", path);
+
+    FILE* fptr;
+    fptr = fopen(path, "r");
+
+    if (fptr == NULL) {
+        printf("Error opening the file.\n");
+    }
+    else {
+        printf("File opened successfully.\n");
+    }
+
     return 0;
 }
